6410960638aee4d799c65dc2aa85bd69a4ed2f1f
TienAnNguyen <le3akmailg@gmail.com>
2022-11-17 19:55:57 +0700

    Thuật toán fibonacci đệ quy

---
+++ Code/Recursion/fibonacci.py
@@ -0,0 +1,15 @@
+#Hàm fibonacci vòng lặp
+def fibonacci_iterative(n):
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+#Hàm fibonacci đệ quy
+def fibonacci_recursive(n):
+    if n < 2:
+        return n
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+print(fibonacci_iterative(6))
+print(fibonacci_recursive(6))
