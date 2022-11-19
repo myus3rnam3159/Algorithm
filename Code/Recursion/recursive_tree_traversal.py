21984740065cd3b8d143156f91decb7f391355b1
TienAnNguyen <le3akmailg@gmail.com>
2022-11-19 13:40:44 +0700

    Duyệt qua cây một cách đệ quy

---
+++ Code/Recursion/recursive_tree_traversal.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def preorder_print(root, path=""):
+    """root->left->right"""
+    if root:
+        path += str(root.data) + "-"
+        path = preorder_print(root.left, path)
+        path = preorder_print(root.right, path)
+    return path
+
+
+def inorder_print(root, path=""):
+    """left->root->right"""
+    if root:
+        path = inorder_print(root.left, path)
+        path += str(root.data) + "-"
+        path = inorder_print(root.right, path)
+    return path
+
+
+def postorder_print(root, path=""):
+    """left->right->root"""
+    if root:
+        path = postorder_print(root.left, path)
+        path = postorder_print(root.right, path)
+        path += str(root.data) + "-"
+    return path
+
+
+if __name__ == '__main__':
+    root = Node("F")
+    root.left = Node("D")
+    root.left.left = Node("B")
+    root.left.left.left = Node("A")
+    root.left.left.right = Node("C")
+    root.left.right = Node("E")
+    root.right = Node("I")
+    root.right.left = Node("G")
+    root.right.left.right = Node("H")
+    root.right.right = Node("J")
+
+    print("Preorder:", preorder_print(root))
+    print("Inorder:", inorder_print(root))
+    print("Postorder", postorder_print(root))
